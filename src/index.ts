91d02ff7f3afceff7a9b46f5691c9343a6014762
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2024-01-12 12:33:24 +0545

    cors error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import authRouter from "./routes/auth.route";
 import userRouter from "./routes/user.route";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
