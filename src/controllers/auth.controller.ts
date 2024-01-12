984b7382f1970bfe6568873335746369c1e684a6
Gyaneshwar Sah <gyaneshwar585@gmail.com>
2024-01-12 12:31:24 +0545

    prisma error handling

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import prisma from "../db";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
+import { Prisma } from "@prisma/client";
 
 const signupUser = async (req: Request, res: Response) => {
   const { fullName, email, password } = req.body;
@@ -29,6 +30,11 @@ const signupUser = async (req: Request, res: Response) => {
 
     return res.status(201).json({ message: "User created", token });
   } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        return res.status(400).json({ message: "User already exists" });
+      }
+    }
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
@@ -69,6 +75,11 @@ const loginUser = async (req: Request, res: Response) => {
 
     return res.status(200).json({ message: "User logged in", token });
   } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return res.status(400).json({ message: "User does not exist" });
+      }
+    }
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
